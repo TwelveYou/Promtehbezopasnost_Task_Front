290a3f73ff7386815eb1d316a63e7f0140bacd8e
Ilya Kuzimn <ilyacool@mail.ru>
2023-03-13 09:23:26 +0700

    Clear component "app" and Add empty component "filmcard"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilmcardComponent } from './filmcard/filmcard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmcardComponent
   ],
   imports: [
     BrowserModule,
